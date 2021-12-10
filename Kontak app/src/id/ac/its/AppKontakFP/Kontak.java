473e1472438d2e345dfbde7faa5cdc25dcbea878
DhalMalu <94875890+DhalMalu@users.noreply.github.com>
2021-12-10 20:13:25 +0700

    update interface kontak

--- Kontak app/src/id/ac/its/AppKontakFP/Kontak.java
+++ Kontak app/src/id/ac/its/AppKontakFP/Kontak.java
@@ -56,7 +56,53 @@ public class Kontak extends JFrame {
 		contentPane.setLayout(null);
 		
 		//Bagian Afdhal
-
+		JLabel nomerL = new JLabel("No Telp");
+		nomerL.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		nomerL.setBounds(47, 31, 57, 23);
+		contentPane.add(nomerL);
+		
+		JLabel namaL = new JLabel("Nama");
+		namaL.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		namaL.setBounds(47, 87, 57, 23);
+		contentPane.add(namaL);
+		
+		JLabel orgL = new JLabel("Organisasi");
+		orgL.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		orgL.setBounds(47, 141, 70, 23);
+		contentPane.add(orgL);
+		
+		JLabel jenisL = new JLabel("Jenis no");
+		jenisL.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		jenisL.setBounds(47, 195, 70, 23);
+		contentPane.add(jenisL);
+		
+		nomerF = new JTextField();
+		nomerF.setBackground(Color.WHITE);
+		nomerF.setEditable(false);
+		nomerF.setColumns(10);
+		nomerF.setBounds(131, 33, 201, 23);
+		contentPane.add(nomerF);
+		
+		namaF = new JTextField();
+		namaF.setBackground(Color.WHITE);
+		namaF.setEditable(false);
+		namaF.setColumns(10);
+		namaF.setBounds(131, 89, 201, 23);
+		contentPane.add(namaF);
+		
+		orgF = new JTextField();
+		orgF.setBackground(Color.WHITE);
+		orgF.setEditable(false);
+		orgF.setColumns(10);
+		orgF.setBounds(131, 145, 201, 23);
+		contentPane.add(orgF);
+		
+		jenisF = new JTextField();
+		jenisF.setBackground(Color.WHITE);
+		jenisF.setEditable(false);
+		jenisF.setColumns(10);
+		jenisF.setBounds(131, 199, 201, 23);
+		contentPane.add(jenisF);
 		
 		
 	}
